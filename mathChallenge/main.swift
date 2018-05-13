43544ced026fc3a14362f29300bfe111c131f44f
Sean Conrad <spconrad@gmail.com>
2018-05-13 19:23:47 -0400

    Initial Commit

---
+++ mathChallenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mathChallenge
+//
+//  Created by Sean Conrad on 5/13/18.
+//  Copyright © 2018 Sean Conrad. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
